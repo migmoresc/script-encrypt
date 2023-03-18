0fb5a703546a6444843dc2efa9cfb60ee806f59d
Miguel Moreno <migmoresc@outlook.es>
2023-03-18 19:44:25 +0100

    Versión 1.1

--- README.md
+++ README.md
@@ -1,4 +1,31 @@
 # script-encrypt
 Script para encriptar archivos
 
+¿Cómo se usa?
+
+Desde la terminal escribiremos para encriptar los archivos:
+
+	python script-encrypt.py -e micontraseña nombreArchivo1.html nombreArchivo2.html ...
+
+Y para desencriptar:
+
+	python script-encrypt.py -d micontraseña nombreArchivo1.html nombreArchivo2.html ...
+
+
 No acepto pull request / I don't accept pull request
+
+CHANGELOG
+
+Versión 1.1
+
+	Código refactorizado
+	Añadido main
+
+Versión 1.0
+
+	Programa que funciona y sólo encripta html
+
+TO DO
+
+- Encriptar según la extensión del archivo
+- Añadir la lógica para otras extensiones como css, js ...
--- script_encrypt.py
+++ script_encrypt.py
@@ -1,4 +1,4 @@
-""" Encripta y desencripta el contenido de los archivos comunes """
+""" Encripta y desencripta el contenido de los ficheros comunes """
 
 import sys
 import os
@@ -6,110 +6,106 @@ import base64
 from cryptography.fernet import Fernet
 
 
-def convierte_linea(linea, modo):
-    """ Función que encripta o desencripta una cadena de texto """
-    if modo == "-e":
-        texto = f.encrypt(linea.encode())
-    elif modo == "-d":
-        texto = f.decrypt(linea)
+def main():
+    """ Función principal """
+    ayuda = ["-h", "--h", "/h", "-help", "--help", "/help"]
+    comandos = ["-e", "-d"]
+    html = ["DOCTYPE", "html", "head", "title", "body", "header",
+            "main", "nav", "article", "aside", "footer"]
+    css = []
+    javascript = []
+    extensiones = [html, css, javascript]
 
-    return texto
+    print('Number of arguments:', len(sys.argv), 'arguments.')
+    print('Argument List:', str(sys.argv))
 
-
-def desen(ficheros, modo):
-    """Encripta los ficheros"""
-
-    for fichero in ficheros:
-        fichero = fichero.replace(".\\", "")
-        if os.stat(fichero).st_size > 0:
-            with open("#"+fichero, "w", encoding="utf-8") as fichero_nuevo:
-                with open(fichero, "rb") as fichero_lectura:
-                    for linea in fichero_lectura:
-                        try:
-                            if linea == b"\n":
-                                linea = linea.decode("utf-8")
-                                fichero_nuevo.write(linea)
-                            else:
-                                salto = 0
-                                if b"\n" in linea:
-                                    linea = linea[:-1]
-                                    salto = 1
-                                esta = False
-                                for palabra in html:
-                                    if palabra.encode() in linea:
-                                        esta = True
-                                        break
-                                if esta is True:
-                                    linea = linea.decode()+"\n"*salto
-                                    fichero_nuevo.write(linea)
-                                else:
-                                    le = convierte_linea(linea, modo)
-                                    fichero_nuevo.write(le.decode()+"\n")
-                        except:  # pylint: disable=W0702
-                            le = convierte_linea(linea, modo)
-                            fichero_nuevo.write(le.decode())
-                fichero_lectura.close()
-            fichero_nuevo.close()
-        else:
-            print("El fichero está vacío")
-
-
-def abrir_ficheros(ficheros, modo):
-    """Encripta los ficheros"""
-    for fichero in ficheros:
-        fichero = fichero.replace(".\\", "")
-        if os.stat(fichero).st_size > 0:
-            with open("encriptado-"+fichero, "wb") as fichero_nuevo:
-                with open(fichero, "r", encoding="utf-8") as fichero_lectura:
-                    for linea in fichero_lectura:
-                        if linea == "\n":
-                            le = linea.encode("utf-8")
-                            fichero_nuevo.write(le)
-                        else:
-                            esta = False
-                            for palabra in html:
-                                if palabra in linea:
-                                    esta = True
-                                    break
-                            if esta is True:
-                                le = linea.encode("utf-8")
-                                fichero_nuevo.write(le)
-                            else:
-                                if "\n" in linea:
-                                    linea = linea[:-1]
-                                le = convierte_linea(linea, modo)
-                                le = le + b"\n"
-                                fichero_nuevo.write(le)
-                fichero_lectura.close()
-            fichero_nuevo.close()
-        else:
-            print("El fichero está vacío")
-
-
-ayuda = ["-h", "--h", "/h", "-help", "--help", "/help"]
-comandos = ["-e", "-d"]
-html = ["DOCTYPE", "html", "head", "title", "body", "header",
-        "main", "nav", "article", "aside", "footer"]
-
-print('Number of arguments:', len(sys.argv), 'arguments.')
-print('Argument List:', str(sys.argv))
-
-if (len(sys.argv) == 1 or sys.argv[1] in ayuda or sys.argv[1] not in comandos or
-        len(sys.argv[3:]) == 0):
-    print("""Los argumentos de este script son:
-    Primer argumento: -e (encriptar) o -d (desencriptar)
-    Segundo argumento: contraseña
-    Siguientes argumentos: nombres de los archivos separados por espacio""")
-else:
-    PASW = str.encode(sys.argv[2]+("0"*(32-len(sys.argv[2]))))
-    ficheros = sys.argv[3:]
-    a = base64.b64encode(PASW)
-    f = Fernet(a)
-
-    if sys.argv[1] == "-e":
-        abrir_ficheros(ficheros, "-e")
+    if (len(sys.argv) < 4 or sys.argv[1] in ayuda or sys.argv[1] not in comandos or
+            len(sys.argv[3:]) == 0):
+        print("""Los argumentos de este script son:
+        Primer argumento: -e (encriptar) o -d (desencriptar)
+        Segundo argumento: contraseña
+        Siguientes argumentos: nombres de los ficheros separados por espacio""")
     else:
-        desen(ficheros, "-d")
+        password = str.encode(sys.argv[2]+("0"*(32-len(sys.argv[2]))))
+        ficheros = sys.argv[3:]
+        password = base64.b64encode(password)
+        fernet = Fernet(password)
 
-# os.remove(ficheros[0])
-# os.rename("##index.html", "html")
+        procesar_ficheros(ficheros, extensiones, sys.argv[1], fernet)
+
+
+def procesar_ficheros(ficheros, extensiones, modo, fernet):
+    """Encripta los ficheros"""
+
+    for fichero in ficheros:
+        fichero = fichero.replace(".\\", "")
+        if os.path.exists(fichero):
+            if os.path.isfile(fichero):
+                if os.stat(fichero).st_size > 0:
+                    if modo == "-e":
+                        with open("encriptado-"+fichero, "wb") as fichero_nuevo:
+                            with open(fichero, "r", encoding="utf-8") as fichero_lectura:
+                                encripta(fichero_lectura,
+                                         fichero_nuevo, extensiones[0], fernet)
+                    else:
+                        if "encriptado-" in fichero:
+                            fichero_nuevo = fichero.replace(
+                                "encriptado-", "desencriptado-")
+                        with open(fichero_nuevo, "w", encoding="utf-8") as fichero_nuevo:
+                            with open(fichero, "rb") as fichero_lectura:
+                                desencripta(fichero_lectura,
+                                            fichero_nuevo, extensiones[0], fernet)
+                else:
+                    print(f'El fichero "{fichero}" está vacío.')
+            else:
+                print(f'El fichero "{fichero}" no existe. ¿Puede ser que '
+                      'hayas introducido el nombre de una carpeta?')
+        else:
+            print(f'No existe ningún fichero con el nombre "{fichero}".')
+
+
+def encripta(fichero_lectura, fichero_nuevo, palabras, fernet):
+    """ Recorre todas las linea del fichero y lo encripta en el nuevo fichero """
+    for linea in fichero_lectura:
+        if linea == "\n":
+            fichero_nuevo.write(linea.encode("utf-8"))
+        else:
+            esta = False
+            for palabra in palabras:
+                if palabra in linea:
+                    esta = True
+                    break
+            if esta is True:
+                fichero_nuevo.write(linea.encode("utf-8"))
+            else:
+                if "\n" in linea:
+                    linea = linea[:-1]
+                fichero_nuevo.write(fernet.encrypt(linea.encode())+b"\n")
+
+
+def desencripta(fichero_lectura, fichero_nuevo, palabras, fernet):
+    """ Recorre todas las linea del fichero encriptado y lo desencripta en el nuevo fichero """
+    for linea in fichero_lectura:
+        try:
+            if linea == b"\n":
+                fichero_nuevo.write(linea.decode("utf-8"))
+            else:
+                salto = 0
+                if b"\n" in linea:
+                    linea = linea[:-1]
+                    salto = 1
+                esta = False
+                for palabra in palabras:
+                    if palabra.encode() in linea:
+                        esta = True
+                        break
+                if esta is True:
+                    fichero_nuevo.write(linea.decode()+"\n"*salto)
+                else:
+                    fichero_nuevo.write(fernet.decrypt(linea).decode()+"\n")
+        except:  # pylint: disable=W0702
+            fichero_nuevo.write(fernet.decrypt(linea).decode())
+
+
+if __name__ == "__main__":
+    main()
